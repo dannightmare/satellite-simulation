d7a13e553516e6912a0b741b255180dcf6ed5995
Daniel Kusai <dan.kusai@gmail.com>
2024-06-24 22:28:12 +0300

    Added colors to histogram
    Signed-off-by: Daniel Kusai <dan.kusai@gmail.com>

--- history.py
+++ history.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt("history.txt", delimiter=",")
 
+
 print(np.unique(data[:, 1]))
 
-plt.hist(data[:, 1], bins=[5, 6, 7])
+plt.hist(data[data[:, 1] == 5, 1], bins=[5, 6, 7], color='orange')
+plt.hist(data[data[:, 1] == 6, 1], bins=[5, 6, 7], color='cyan')
 plt.xticks([5, 6], horizontalalignment='left')
 plt.title('Number of satellite hops (TelHai - NY)')
 plt.ylabel('number of simulations')
 plt.show()
 
-plt.hist(data[:, 0] / 3)
+plt.hist(data[:, 0] / 3, color='turquoise')
 plt.title('latency (TelHai - NY)')
 plt.ylabel('number of simulations')
 plt.xlabel('miliseconds (ms)')
--- historyNS.py
+++ historyNS.py
@@ -6,7 +6,9 @@ data = np.genfromtxt("historyNS.txt", delimiter=",")
 
 print(np.unique(data[:, 1]))
 
-plt.hist(data[:, 1], bins=[8, 9, 10, 11])
+plt.hist(data[data[:, 1] == 8, 1], bins=[8, 9, 10, 11], color='orange')
+plt.hist(data[data[:, 1] == 9, 1], bins=[8, 9, 10, 11], color='magenta')
+plt.hist(data[data[:, 1] == 10, 1], bins=[8, 9, 10, 11], color='blue')
 plt.xticks([8, 9, 10], horizontalalignment='left')
 plt.title('Number of satellite hops (North - South poles)')
 plt.ylabel('number of simulations')
